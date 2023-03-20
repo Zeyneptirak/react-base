4fe3323a5b6cc948e3dba772cd1d25b85b2a7a12
Zeyneptirak <zeyneptirak33@gmail.com>
2023-03-20 14:26:59 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
+import store, { persistor } from "./Store/Store";
+import * as _redux from "./Store/index";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import RouterPage from "./routes/RouterPage";
+
+_redux.setupAxios(axios, store);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={"Loading"} persistor={persistor}>
+        <React.StrictMode>
+         <RouterPage/>
+        </React.StrictMode>
+      </PersistGate>
+    </Provider>
   );
 }
 
